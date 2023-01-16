276133795a763fe5b9cf62c5569492952c55acea
Jesus-QC <jesusquirocampos@gmail.com>
2023-01-16 21:54:04 +0100

    Initial commit

---
+++ UncappedSlots/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace UncappedSlots;
+
+public class EntryPoint
+{
+    public const string Version = "1.0.0.0";
+    
+    private static readonly Harmony HarmonyPatcher = new("uncappedslots.jesusqc.com");
+
+    [PluginEntryPoint("UncappedSlots", Version, "Removes slot limits by hosting providers.", "Jesus-QC")]
+    private void Init()
+    {
+        Log.Raw($"<color=blue>Loading UncappedSlots {Version} by Jesus-QC</color>");
+        HarmonyPatcher.PatchAll();
+    }
+}
---
+++ UncappedSlots/SlotPatch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using NorthwoodLib.Pools;
+
+namespace UncappedSlots;
+
+[HarmonyPatch(typeof(CustomNetworkManager), nameof(CustomNetworkManager.CreateMatch))]
+public class SlotPatch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+        
+        int index = newInstructions.FindLastIndex(x => x.opcode == OpCodes.Callvirt && x.operand is MethodInfo info && info == AccessTools.Method(typeof(YamlConfig), nameof(YamlConfig.GetInt)));
+
+        newInstructions[index].operand = AccessTools.Method(typeof(SlotPatch), nameof(UncappedSlots));
+        
+        foreach (CodeInstruction instruction in newInstructions)
+            yield return instruction;
+        
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+
+    private static int UncappedSlots(YamlConfig _, string key, int def) => -1;
+}
